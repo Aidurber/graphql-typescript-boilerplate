dbbc494231175cafa023b1203f21273e29baae93
Andrew Durber <aidurber@gmail.com>
2018-11-24 18:17:40 +0000

    Refactor TypeOrm connection

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,13 @@ import 'reflect-metadata';
 import { Container } from 'typedi';
 import * as TypeORM from 'typeorm';
 import * as TypeGraphQL from 'type-graphql';
-import entities from './entities';
 import createServer from './createServer';
 
 TypeGraphQL.useContainer(Container);
 TypeORM.useContainer(Container);
 
 async function bootstrap() {
-  await TypeORM.createConnection({
-    type: 'mysql',
-    database: 'type-graphql',
-    username: 'root', // fill this with your username
-    password: '', // and password
-    port: 3306,
-    host: 'localhost',
-    entities,
-    synchronize: true,
-    logger: 'advanced-console',
-    logging: 'all',
-    dropSchema: true,
-    cache: true
-  });
+  await TypeORM.createConnection();
 
   const server = await createServer();
   server.start({}, deets => {
